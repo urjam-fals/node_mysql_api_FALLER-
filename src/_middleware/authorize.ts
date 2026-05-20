301d76530bce24379dd71dd879ab4949d18fde3d
Urjam Fals <jumarfal45@gmail.com>
2026-05-20 21:18:09 +0800

    Commit with authorize.ts, error-handler.ts, validate-request.ts

---
+++ src/_middleware/authorize.ts
@@ -0,0 +1,27 @@
+import { expressjwt } from 'express-jwt';
+import db from '../_helpers/db';
+import config from '../../config.json';
+
+const {secret} = config;
+
+export default function authorize(roles: any = []){
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+
+    return [
+        expressjwt({ secret, algorithms: ['HS256'] }),
+        async (req: any, res: any, next: any) => {
+            const account = await db.Account.findByPk(req.user.id);
+
+            if(!account || (roles.length && !roles.includes(account.role))) {
+                return res.status(401).json({message: 'Unauthorized'});
+            }
+
+            req.user.role = account.role;
+            const refreshTokens = await account.getRefreshToken();
+            req.user.ownsToken = (token: any) => !!refreshTokens.find((x:any) => x.token === token);
+            next();
+        }
+    ];
+}
